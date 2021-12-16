c0015efbec5475f5036de2f23072536d56b53cdd
Sasha Samoilov <samsiki50@gmail.com>
2021-12-17 01:07:20 +0300

    NewLabs

---
+++ Labs/Lab1/Part1/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	double x, y, c;
+	cout << "Enter x ";
+	cin >> x;
+	cout << "Enter y ";
+	cin >> y;
+	c = x * (x - 20) + (3 * y) / (x);
+	cout << "Anser = " << c << endl;
+	system("pause");
+	return 0;
+}
---
+++ Labs/Lab1/Part2/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	double b, c;
+	b = 1;
+	c = 4;
+	double a, x, y;
+	cout << "Enter a";
+	cin >> a;
+	cout << "Enter x";
+	cin >> x;
+	y = pow(x, 2) - a * x + b - c;
+
+	cout << "Answer =" << y << endl;
+	system("pause");
+	return 0;
+}
